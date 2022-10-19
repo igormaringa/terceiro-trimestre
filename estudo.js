f1e5a99d4701a8e19100222801789b18ec0766a5
igormaringa <114078208+igormaringa@users.noreply.github.com>
2022-10-19 10:00:37 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -36,3 +36,13 @@ function mat (){
 
 
     document.write("Resultado " + r);
+
+    function media (){
+        let n1 = document. getElementById("v1").value;
+        let n2= document. getElementById("v2").value;
+        let n3 = document. getElementById("v3").value;
+        let r = (Number(n1) + Number (n2) + Number (n3))/3;
+        document. getElementById("resultado"). innerHTML = "Média" + r;
+        
+
+    
